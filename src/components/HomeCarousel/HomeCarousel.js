5e7b0fb97b804368408c10286ee755f7465d7e80
Robiya <77631787+Robiyaxon@users.noreply.github.com>
2022-03-05 21:21:21 +0500

    news

--- src/components/HomeCarousel/HomeCarousel.js
+++ src/components/HomeCarousel/HomeCarousel.js
@@ -3,39 +3,45 @@ import { Carousel } from "react-bootstrap";
 import "./HomeCarousel.scss";
 
 function HomeCarousel() {
+  const map = [
+    {
+      id: 1,
+      clas1: "for_backline1",
+      class2: "slider1",
+      title: "QURILISHLAR",
+      title2: "BIZ QURGAN BINOLARNI LOYIHALAR BO'LIMIDA KO'RING",
+    },
+    {
+      id: 2,
+      clas1: "for_backline2",
+      class2: "slider2",
+      title: "QURILISHLAR",
+      title2: "Biz qurgan 10 ta binolarni qanday qurilganini koring",
+    },
+    {
+      id: 3,
+      clas1: "for_backline3",
+      class2: "slider3",
+      title: "QURILISHLAR",
+      title2: "Biz qurgan 10 ta binolarni qanday qurilganini koring",
+    }
+  ];
+  const map2 = map.map((a) => (
+    <Carousel.Item key={a.id}>
+      <div className={a.clas1}>
+        <div className={a.class2}>
+          <div className="sometext">
+            <h1>{a.title}</h1>
+            <h4>{a.title2}</h4>
+          </div>
+        </div>
+      </div>
+    </Carousel.Item>
+  ));
   return (
     <div className="HomeCarousel">
       <Carousel fade>
-        <Carousel.Item>
-          <div className="for_backline1">
-            <div className="slider1">
-              <div className="sometext">
-                <h1>QURILISHLAR</h1>
-                <h4>BIZ QURGAN BINOLARNI LOYIHALAR BO'LIMIDA KO'RING</h4>
-              </div>
-            </div>
-          </div>
-        </Carousel.Item>
-        <Carousel.Item>
-          <div className="for_backline2">
-            <div className="slider2">
-              <div className="sometext">
-                <h1>QURILISHLAR</h1>
-                <h4>Biz qurgan 10 ta binolarni qanday qurilganini koring </h4>
-              </div>
-            </div>
-          </div>
-        </Carousel.Item>
-        <Carousel.Item>
-          <div className="for_backline3">
-            <div className="slider3">
-              <div className="sometext">
-                <h1>QURILISHLAR</h1>
-                <h4>Biz qurgan 10 ta binolarni qanday qurilganini koring </h4>
-              </div>
-            </div>
-          </div>
-        </Carousel.Item>
+      {map2}
       </Carousel>
     </div>
   );
